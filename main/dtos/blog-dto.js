708df95570c8267374803bee99a9536d0b3e359f
omgdendi <a4baytakov@mail.ru>
2022-05-25 20:06:38 +0300

    fix dto

--- main/dtos/blog-dto.js
+++ main/dtos/blog-dto.js
@@ -2,19 +2,19 @@ const UserDTO = require("./user-dto");
 
 
 module.exports = class BlogDTO {
-    id
-    message
-    createdAt
-    author
-    img
-    video
+    id;
+    message;
+    createdAt;
+    author;
+    img;
+    video;
 
     constructor(model) {
-        this.id = model.id
-        this.message = model.message
-        this.createdAt = model.createdAt
-        this.img = model.img
-        this.video = model.video
-        this.author = model.userId
+        this.id = model.id;
+        this.message = model.message;
+        this.createdAt = model.createdAt;
+        this.img = model.img;
+        this.video = model.video;
+        this.author = model.userId;
     }
 }
